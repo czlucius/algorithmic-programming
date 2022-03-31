a5ea01e4235ce65ab5ea887cf229b6fbd1262b88
Lucius C <58442255+czlucius@users.noreply.github.com>
2022-03-31 20:34:32 +0800

    Create template.cc

---
+++ codejam/template.cc
@@ -0,0 +1,42 @@
+/**
+C++ template for Google Code Jam (non-interactive
+*/
+#include <iostream>
+// #ifndef GOOGLE_CODE_JAM
+
+/**
+ * Use to debug your code.
+ * Use like cout, like this:
+ * debug << "Some text";
+ * debug << someVar;
+ * 
+ * To disable the log statements, change 
+ * #if 1
+ * to:
+ * #if 0
+ */
+#if 1
+#define debug cout
+#else
+#define debug 0 && cout
+#endif
+
+using namespace std;
+
+
+
+int main()
+{
+    // Multiple test cases will be supplied. Read from testCases to get no. of testCases
+    int testCases; cin >> testCases;
+    
+    for (int i = 1; i <= testCases; i++) {
+        // Do your code every test case here.
+        
+        
+        auto answer = 99; // Please modify this to be whatever answer obtained.
+        cout << "Case #" << i << ": " << answer << endl;
+    }
+    
+    return 0;
+}
