33ff28fa0ad390347c26deb12770a532403d4eaa
Lucius C <58442255+czlucius@users.noreply.github.com>
2022-03-31 20:36:49 +0800

    Add explainer comments

--- codejam/template.py
+++ codejam/template.py
@@ -1,6 +1,8 @@
 """
 Python template for Google Code Jam (non-interactive)
 """
+# Use to debug your code. 
+# Use it like print, it will pass all arguments to print (including positional args so you do not have to concatenate and convert values)
 def debug(vals, *pvals, **kwvals):
     print(vals, *pvals, **kwvals) # Comment this out when doing actual run (so this fn results in no-op). For printing of debug statements.
     pass
