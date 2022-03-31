7f94309619b2a3485f8367c4ad0996d857ca55ee
Lucius C <58442255+czlucius@users.noreply.github.com>
2022-03-31 20:14:05 +0800

    Create template.py

---
+++ codejam/template.py
@@ -0,0 +1,13 @@
+
+def debug(vals, *pvals, **kwvals):
+    print(vals, *pvals, **kwvals) # Comment this out when doing actual run (so this fn results in no-op). For printing of debug statements.
+    pass
+
+testCases = int(input())
+
+for testCase in range(1, testCases + 1):
+    
+    
+    
+    answer = 0 # Please modify this to be whatever answer obtained.
+    print(f"Case #{testCase}: {answer}")
