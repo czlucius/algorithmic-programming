1cb81285c6c3f05d0c6385161a8f8d00a573ecc3
Lucius C <58442255+czlucius@users.noreply.github.com>
2022-03-31 21:03:00 +0800

    Create template.kt

---
+++ codejam/template.kt
@@ -0,0 +1,27 @@
+/**
+Kotlin template for Google Code Jam (non-interactive)
+*/
+
+/**
+ * Use to debug your code.
+ * Use like println.
+ */
+fun debug(value: Any?) {
+  println(value) // Comment this line to disable debug logging.
+}
+
+
+
+fun main(args: Array<String>) {
+  val testCases = readLine()!!.toInt()
+
+  for (i in 1..testCases) {
+    debug("Hello World")
+
+
+
+    var answer = 0
+    println("Case #$i: $answer")
+  }
+  
+}
